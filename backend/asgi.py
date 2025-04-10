f0455352017797473b1df62956cd0aaffba835d0
sundaysmauel250 <orinamesunday360@gmail.com>
2025-04-10 13:43:12 -0700

    module error

--- backend/asgi.py
+++ backend/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
