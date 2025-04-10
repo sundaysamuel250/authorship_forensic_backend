7188ac9fe9b60a1dadaa86f2885b8536b39e99c4
sundaysmauel250 <orinamesunday360@gmail.com>
2025-04-10 06:23:04 -0700

    changes from deployment file

--- api/deployment_settings.py
+++ api/deployment_settings.py
@@ -1,6 +1,6 @@
 import os 
 import dj_database_url
-from .settings import * 
+from backend.settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
