9b8f2e89e6555a95f9d2843dd40955991a5a98c2
sundaysmauel250 <orinamesunday360@gmail.com>
2025-04-10 12:50:02 -0700

    changes from secret key

--- api/deployment_settings.py
+++ api/deployment_settings.py
@@ -37,7 +37,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= os.environ['DATABASE_URL'], 
+        default= os.environ('DATABASE_URL'), 
         conn_max_age=600
     )
 }
--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'backend.api',
+    'api',
     'corsheaders',
     'rest_framework',
     'django.contrib.staticfiles',
